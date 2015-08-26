185313117c670bc9809e5eb3fc1471c2b47700d0
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2015-08-25 20:30:20 -0400

    Changed a few things: * Rebuilt config.js. * Removed undesired code from Startup.cs. * Setup the usage of RequireJS.  * Created a simple demo using some unfamiliar ES6 techniques.

--- src/AureliaHelloWorld/Startup.cs
+++ src/AureliaHelloWorld/Startup.cs
@@ -17,10 +17,6 @@ namespace AureliaHelloWorld
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
         }
     }
 }
---
+++ src/AureliaHelloWorld/wwwroot/app/greeting.js
@@ -0,0 +1,8 @@
+﻿import moment from 'moment';
+
+export default element => {
+	let pleasantry = 'Helloz';
+	let timeLeft = moment().startOf('hour').fromNow();
+
+	element.innerText = `${pleasantry} The hour started ${timeLeft}`;
+};
