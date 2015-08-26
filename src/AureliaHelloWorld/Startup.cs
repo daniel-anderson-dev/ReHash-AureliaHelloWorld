3565260153262200ac558981003c85629bf9bc66
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2015-08-25 20:57:54 -0400

    * Installed AspNet.StaticFiles. * Setup the default within Startup.cs.

--- src/AureliaHelloWorld/Startup.cs
+++ src/AureliaHelloWorld/Startup.cs
@@ -5,18 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.StaticFiles;
+
 
 namespace AureliaHelloWorld
 {
     public class Startup
     {
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-        }
-    }
+		public void Configure(IApplicationBuilder app)
+		{
+			app.UseFileServer(new FileServerOptions
+			{
+				EnableDefaultFiles = true,
+				EnableDirectoryBrowsing = true
+			});
+		}
+	}
 }
